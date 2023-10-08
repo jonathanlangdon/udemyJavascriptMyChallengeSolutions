0667247b2d1f672a7ba608e25881df98a4991384
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-10-08 13:04:00 -0400

    events added

--- 94-107 shopping-list-start/script.js
+++ 94-107 shopping-list-start/script.js
@@ -4,7 +4,13 @@ const itemList = document.getElementById('item-list')
 const clearBtn = document.getElementById('clear')
 const itemFilter = document.getElementById('filter')
 
-function addItem(e) {
+function displayItems() {
+  const itemsFromStorage = getItemsFromStorage()
+  itemsFromStorage.forEach(item => addItemToDOM(item))
+  checkUI()
+}
+
+function onaddItemSubmit(e) {
   e.preventDefault()
 
   // Validate Input
@@ -14,20 +20,40 @@ function addItem(e) {
   }
 
   const newItem = itemInput.value
-  const li = document.createElement('li')
-  const button = document.createElement('button')
-  const icon = document.createElement('i')
-
-  button.classList = 'remove-item btn-link text-red'
-  button.appendChild(icon)
-  icon.classList = 'fa-solid fa-xmark'
-  li.innerText = newItem
-  li.appendChild(button)
-  itemList.appendChild(li)
-
+  addItemToDOM(newItem)
+  addItemToStorage(newItem)
   checkUI()
 
   itemInput.value = ''
+}
+
+function addItemToDOM(item) {
+  const li = document.createElement('li')
+  const button = document.createElement('button')
+  const icon = document.createElement('i')
+  button.classList = 'remove-item btn-link text-red'
+  button.appendChild(icon)
+  icon.classList = 'fa-solid fa-xmark'
+  li.innerText = item
+  li.appendChild(button)
+  itemList.appendChild(li)
+}
+
+function addItemToStorage(item) {
+  const itemsFromStorage = getItemsFromStorage()
+  itemsFromStorage.push(item)
+  localStorage.setItem('items', JSON.stringify(itemsFromStorage))
+}
+
+function getItemsFromStorage() {
+  let itemsFromStorage
+
+  if (localStorage.getItem('items') === null) {
+    itemsFromStorage = []
+  } else {
+    itemsFromStorage = JSON.parse(localStorage.getItem('items'))
+  }
+  return itemsFromStorage
 }
 
 function removeItem(e) {
@@ -48,6 +74,20 @@ function clearItems(e) {
   }
 }
 
+function filterItems(e) {
+  const text = e.target.value.toLowerCase()
+  const items = itemList.querySelectorAll('li')
+
+  items.forEach(item => {
+    const itemName = item.firstChild.textContent.toLowerCase()
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = 'flex'
+    } else {
+      item.style.display = 'none'
+    }
+  })
+}
+
 function checkUI() {
   const items = itemList.querySelectorAll('li')
   if (items.length === 0) {
@@ -59,9 +99,15 @@ function checkUI() {
   }
 }
 
-// Event Listeners
-itemForm.addEventListener('submit', addItem)
-itemList.addEventListener('click', removeItem)
-clearBtn.addEventListener('click', clearItems)
+function init() {
+  // Event Listeners
+  itemForm.addEventListener('submit', onaddItemSubmit)
+  itemList.addEventListener('click', removeItem)
+  clearBtn.addEventListener('click', clearItems)
+  itemFilter.addEventListener('input', filterItems)
+  document.addEventListener('DOMContentLoaded', displayItems)
+  displayItems()
+  checkUI()
+}
 
-checkUI()
+init()
